ffbb2ead38e822dd519ae3a7195591bd17437c8f
Mohamed Alaa <mohamed.alaa.elhefny@gmail.com>
2024-12-25 14:48:04 +0200

    update baseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:4200';
+const baseUrl = 'https://wajbaapi-08765bdfe115.herokuapp.com/';
 
 export const environment = {
   production: false,
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://localhost:44330/',
+    issuer: baseUrl,
     redirectUri: baseUrl,
     clientId: 'Wajba_App',
     responseType: 'code',
@@ -19,7 +19,7 @@ export const environment = {
   },
   apis: {
     default: {
-      url: 'https://localhost:44330',
+      url: baseUrl,
       rootNamespace: 'Wajba',
     },
   },
